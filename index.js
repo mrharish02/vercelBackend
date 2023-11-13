18f30460c22a7411b56091c1bade004e6365e351
nehakumari02 <b20115@students.iitmandi.ac.in>
2023-11-14 00:46:33 +0530

    vercel config

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const url = "mongodb+srv://usersdatabase:FGBhXp36eG0fO0dy@cluster0.f4yku3x.mongo
 
 const app = express();
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'https://vercel-frontend-five.vercel.app/', methods: ['GET', 'POST', 'PUT', 'DELETE'], }));
 app.use(cookieParser());
 
 
@@ -32,26 +32,26 @@ app.post('/', async(req, res) => {
 
         if (user == null) {
             console.log("Credentials Not found");
-            res.json({status:'nouser',user:false})
+            res.json({ status: 'nouser', user: false })
         } else if (req.body.password == user["password"]) {
-            
-                console.log("Verified");
-                console.log(user);
-                const token = jwt.sign({email:user.email,name:user.name},'thisissecuredtransmissionofpasswordsandusernameandnoonecanaccessitatanycostwhatsoeveritisconfidential',{expiresIn:'1h'})
-                res.cookie('jwt',token,{httpOnly:true})
-                res.json({status:'success',user:true})
+
+            console.log("Verified");
+            console.log(user);
+            const token = jwt.sign({ email: user.email, name: user.name }, 'thisissecuredtransmissionofpasswordsandusernameandnoonecanaccessitatanycostwhatsoeveritisconfidential', { expiresIn: '1h' })
+            res.cookie('jwt', token, { httpOnly: true })
+            res.json({ status: 'success', user: true })
         } else {
             console.log("Invalid Details");
-            res.json({status:'invalid',user:false})
+            res.json({ status: 'invalid', user: false })
         }
         client.close();
     } catch (err) {
         console.error("Error connecting to MongoDB:", err);
-        res.json({status:'error',user:false})
+        res.json({ status: 'error', user: false })
     }
 });
 
-app.post('/register', async (req, res) => {
+app.post('/register', async(req, res) => {
     try {
         const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
         await client.connect();
@@ -65,8 +65,7 @@ app.post('/register', async (req, res) => {
         if (existingUser) {
             console.log('Duplicate email found');
             return res.json({ status: 'emailpresentalready', error: 'Duplicate email' });
-        }
-        else{
+        } else {
             const user = {
                 name: req.body.name,
                 email: req.body.email,
@@ -91,33 +90,33 @@ app.post('/home', async(req, res) => {
     if (!token) {
         return res.json({ status: 404 });
     }
-    try{
+    try {
         const decoded = jwt.verify(token, 'thisissecuredtransmissionofpasswordsandusernameandnoonecanaccessitatanycostwhatsoeveritisconfidential');
         const id = decoded.email;
         console.log(id)
         todo = {
-            userId:id,
-            title:req.body.title,
-            desc:req.body.desc,
+            userId: id,
+            title: req.body.title,
+            desc: req.body.desc,
         }
         console.log(todo)
-        const client = new MongoClient(url,{ useNewUrlParser: true, useUnifiedTopology: true })
+        const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true })
         await client.connect();
         console.log("Connected to the database");
         const db = client.db('User-Data');
 
         const toDos = db.collection('Todo-Details')
         const updateResult = await toDos.insertOne(todo)
-        if(updateResult){
+        if (updateResult) {
             console.log('todo created')
-            res.json({status:'todocreated'})
+            res.json({ status: 'todocreated' })
         }
-    }catch(err){
-        res.json({status:'error',error:'Duplicate email'})
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
     }
 });
 
-app.get('/todos', async (req, res) => {
+app.get('/todos', async(req, res) => {
     const token = req.cookies.jwt;
     if (!token) {
         return res.json({ status: 404 });
@@ -139,7 +138,7 @@ app.get('/todos', async (req, res) => {
     }
 });
 
-app.delete('/todos/:title', async (req, res) => {
+app.delete('/todos/:title', async(req, res) => {
     const token = req.cookies.jwt;
     if (!token) {
         return res.json({ status: 404 });
@@ -170,17 +169,17 @@ app.delete('/todos/:title', async (req, res) => {
 });
 
 app.post('/logout', (req, res) => {
-  // Clear the JWT cookie by setting an empty value and an expired date
-  res.clearCookie('jwt', { httpOnly: true, expires: new Date(0) });
+    // Clear the JWT cookie by setting an empty value and an expired date
+    res.clearCookie('jwt', { httpOnly: true, expires: new Date(0) });
 
-  // Additional cleanup or logout actions, if any
-  // For example, you can invalidate the JWT token on the server-side, if necessary
+    // Additional cleanup or logout actions, if any
+    // For example, you can invalidate the JWT token on the server-side, if necessary
 
-  // Send a response indicating successful logout
-  res.json({ status: 'success', message: 'Logged out successfully' });
+    // Send a response indicating successful logout
+    res.json({ status: 'success', message: 'Logged out successfully' });
 });
 
 
 app.listen(port, () => {
-    console.log("server is running "+port)
+    console.log("server is running " + port)
 });
